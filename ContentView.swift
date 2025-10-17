decaf2a6e83c906d8a9ca98f5cd09c97367a475c
Jose Manuel Hernandez Mayorga <manuelmayorga99@gmail.com>
2025-10-17 12:29:31 -0600

    Repair project structure

--- ContentView.swift
+++ ContentView.swift
@@ -14,6 +14,9 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Image(systemName: "eye")
+                .imageScale(.medium)
+                .foregroundStyle(.tint)
         }
         .padding()
     }
