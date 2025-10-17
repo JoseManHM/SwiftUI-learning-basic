f357a8dc0c71706663741f6248dbd41635b9ff7e
Jose Manuel Hernandez Mayorga <manuelmayorga99@gmail.com>
2025-10-17 12:03:30 -0600

    Initial Commit

---
+++ LearningSwiftUI/LearningSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftUIApp.swift
+//  LearningSwiftUI
+//
+//  Created by Jose Manuel Hernandez on 17/10/25.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
